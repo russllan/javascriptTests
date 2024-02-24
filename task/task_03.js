055e6d2a64e80f31251d156d00ac7c5d1e615d51
ruslan <lotinsk@gmail.com>
2024-02-24 11:11:09 +0600

    feat: new testing

--- task/task_03.js
+++ task/task_03.js
@@ -1,11 +1,12 @@
 function binarySearch(arr, key) {
+  // const arr = [5, 10, 12, 37];
+  // const key = 37;
+  // console.time('FOR LOOP PROFILE')
     arr.sort((a, b) => a - b);
 
     let left = 0;
     let right = arr.length - 1;
   
-
-    
     while (left <= right) {
       const mid = Math.floor((left + right) / 2);
       
@@ -19,6 +20,12 @@ function binarySearch(arr, key) {
     }
   
     return -1; // Элемент не найден
+    // console.timeEnd('FOR LOOP PROFILE')
   }
   
+//   document.getElementById('searchButton').addEventListener('click', function() {
+//     // Вызов вашей функции бинарного поиска при нажатии на кнопку
+//     binarySearchFunction();
+// });
+
 module.exports = binarySearch;  
